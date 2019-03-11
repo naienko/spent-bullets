462e778eadb6ec57b5cd162775960dbf1a8fecec
Panya Farnette <naienko@gmail.com>
2019-03-11 09:25:27 -0500

    starter files

---
+++ src/SpentBullets.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ApplicationView from "./components/ApplicationViews";
+
+export default class SpentBullets extends Component {
+    render() {
+        return(
+            <React.Fragment>
+                <ApplicationView />
+            </React.Fragment>
+        )
+    }
+}
