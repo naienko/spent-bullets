952459c96539f59690166f235ab2403dc6d9cd7b
Panya Farnette <naienko@gmail.com>
2019-03-11 11:18:07 -0500

    react-bootstrap setup in StorageCloset, awaiting data

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -31,9 +31,12 @@ export default class ApplicationView extends Component {
             //note use of HOC component to make sure all <Route>s are authenticated
             <React.Fragment>
                 <Route path="/login" component={Login} />
+                <Route exact path="/" render={(props) => {
+                    return <StorageCloset />
+                }} />
 
-                <AuthRoute path="/" Destination={StorageCloset} /*some props here */ />
-{/*     
+                {/*     
+                <AuthRoute path="/" Destination={StorageCloset} /*some props here />
                 <AuthRoute path="/stack/new" Destination={StackForm} addStack={this.addStack} />
     
                 <AuthRoute path="/stack/:stackId(\d+)/update" Destination={StackUpdate} updateStack={this.updateStack} /> */}
