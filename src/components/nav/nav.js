fb959aa06efba56817b8fb275594d41dde1ec04e
Panya Farnette <naienko@gmail.com>
2019-03-12 09:38:02 -0500

    find a way to float the logout to the right later

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import "./nav.css"
 
 export default class NavBar extends Component {
     render() {
         return (
             <Navbar fixed="top" className="shadow" id="nav" variant="dark" bg="dark">
                 <Nav fill variant="pills">
+                    <Col>
                     <Nav.Item>
                         <Nav.Link href="/">Home</Nav.Link>
                     </Nav.Item>
+                    </Col>
+                    <Col>
                     <Nav.Item>
                         <Nav.Link href="/stack/new">Add New Stack</Nav.Link>
                     </Nav.Item>
+                    </Col>
+                    <Col>
+                    <Nav.Item>
+                        <Nav.Link>Logout</Nav.Link>
+                    </Nav.Item>
+                    </Col>
                 </Nav>
             </Navbar>
         )
