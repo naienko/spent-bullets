ebbf82f940057527bbf1fb73735b426489c880a0
Panya Farnette <naienko@gmail.com>
2019-03-12 09:14:41 -0500

    nav links now actually work

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -2,15 +2,16 @@ import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
-import "./nav.css";
-
 export default class NavBar extends Component {
     render() {
         return (
-            <Navbar fixed="top" className="shadow" id="nav">
+            <Navbar fixed="top" className="shadow" id="nav" variant="dark" bg="dark">
                 <Nav fill variant="pills">
                     <Nav.Item>
-                        <Nav.Link eventKey="1" to="/stack/new">Add New Stack</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/stack/new">Add New Stack</Nav.Link>
                     </Nav.Item>
                 </Nav>
             </Navbar>
