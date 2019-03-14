cfd59dc481e634cba86455f90b18439c9492bddc
Panya Farnette <naienko@gmail.com>
2019-03-14 10:21:36 -0500

    one group works

--- src/components/StorageCloset.js
+++ src/components/StorageCloset.js
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
-import CardDeck from "react-bootstrap/CardDeck";
+
 import { withRouter } from "react-router";
 
 import "./StorageCloset.css"
 import CaliberGroup from "./stack/CaliberGroup";
 
 class StorageCloset extends Component {
-    
     render () {
         let teststack = this.props.stacks.filter(stack => stack.brandCaliber.caliberId === 4)
-        //this.props.stacks.map(stack => console.log(stack.brandCaliber))
         console.log(teststack);
         return (
             <div id="dashboard" className="d-flex flex-row">
-            { /* data goes here  */ }
-            <CardDeck>
-                <CaliberGroup {...this.props} stack={teststack} brands={this.props.brands} calibers={this.props.calibers} />
-                
-            </CardDeck>
+            
+                <CaliberGroup stacks={this.props.stacks.filter(stack => stack.brandCaliber.caliberId === 4)}
+                    brands={this.props.brands} 
+                    calibers={this.props.calibers} />
+
             </div>
         )
     }
--- src/components/stack/CaliberGroup.js
+++ src/components/stack/CaliberGroup.js
@@ -2,37 +2,40 @@ import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
-
+import CardDeck from "react-bootstrap/CardDeck";
 
 class CaliberGroup extends Component {
     render() {
-        console.log(this.props)
         return (
-            <Card key={this.props.stack.id}>
-                <Card.Body className="text-center">
-                    <Card.Title>
-                        {this.props.stack.amount} <span className="text-muted small">count</span>
-                    </Card.Title>
-                    {/* <Card.Text>
+            <CardDeck>
+            { this.props.stacks.map(stack => 
+                <Card key={stack.id}>
+                    <Card.Body className="text-center">
+                        <Card.Title>
+                            {stack.amount} <span className="text-muted small">count</span>
+                        </Card.Title>
+                    <Card.Text>
                         { this.props.brands.find(
-                            brand => brand.id === this.props.stack.brandCaliber.brandId
-                        ).brand }
+                            brand => brand.id === stack.brandCaliber.brandId
+                            ).brand }
                         {" "}
                         { this.props.calibers.find(
-                            caliber => caliber.id === this.props.stack.brandCaliber.caliberId
-                        ).caliber }
+                            caliber => caliber.id === stack.brandCaliber.caliberId
+                            ).caliber }
                         <br />
-                        { !this.props.stack.notes.length ? <br /> : `notes: ${this.props.stack.notes}` }
-                    </Card.Text> */}
+                        { !stack.notes.length ? <br /> : `notes: ${stack.notes}` }
+                    </Card.Text>
                     <ButtonGroup>
-                        <Button variant="success" onClick={() => { this.props.history.push(`/stack/${this.props.stack.id}/update`); }}>Update</Button>
-                        <Button variant="danger" onClick={() => window.confirm("Are you sure you want to delete this stack?") && this.props.deleteStack(this.props.stack.id)
-                            .then(() => this.props.history.push("/"))}>Remove</Button>
+                        <Button variant="success" onClick={() => { this.props.history.push(`/stack/${stack.id}/update`); }}>Update</Button>
+                        <Button variant="danger" onClick={() => window.confirm("Are you sure you want to delete this stack?") && this.props.deleteStack(stack.id)
+                        .then(() => this.props.history.push("/"))}>Remove</Button>
                     </ButtonGroup>
-                </Card.Body>
-            </Card>
+                    </Card.Body>
+                </Card>
+            )}
+            </CardDeck>
         )
     }
 };
-
+                
 export default CaliberGroup;
