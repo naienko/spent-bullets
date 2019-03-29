fda7da31022d42c51d0e2df055bc73d1ab0bf05e
Panya Farnette <naienko@gmail.com>
2019-03-29 12:35:23 -0500

    user feedback fixes

--- src/components/stack/CaliberGroup.js
+++ src/components/stack/CaliberGroup.js
@@ -12,7 +12,7 @@ class CaliberGroup extends Component {
         return (
             <CardDeck>
                 <Card bg="light" border="info"><h3 className="text-center pt-sm-5">{this.props.type}</h3>
-                <Card.Subtitle className="text-center">{ this.props.stacks.reduce((total_count, stack) => total_count + stack.amount, 0) } <span className="text-muted small">count</span></Card.Subtitle>
+                <Card.Subtitle className="text-center">{ this.props.stacks.reduce((total_count, stack) => parseInt(total_count) + parseInt(stack.amount), 0) } <span className="text-muted small">count</span></Card.Subtitle>
                 </Card>
             { this.props.stacks.map(stack => 
                 <Card key={stack.id}>
@@ -34,7 +34,7 @@ class CaliberGroup extends Component {
                     <ButtonGroup>
                         <Button variant="success" onClick={() => { this.props.history.push(`/stack/${stack.id}/update`); }}>Update</Button>
                         <Button variant="danger" onClick={() => window.confirm("Are you sure you want to delete this stack?") && this.props.deleteStack(stack.id)
-                        .then(() => this.props.history.push("/"))}>Remove</Button>
+                        .then(() => this.props.history.push("/"))}>Delete</Button>
                     </ButtonGroup>
                     </Card.Body>
                 </Card>
