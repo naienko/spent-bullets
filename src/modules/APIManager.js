9a7d18b0d9449a8d5a4db95ea434a39b8f6a86f3
Panya Farnette <naienko@gmail.com>
2019-03-29 20:03:30 -0500

    correct database link

--- src/modules/APIManager.js
+++ src/modules/APIManager.js
@@ -2,8 +2,6 @@ const RemoteURL = `https://bullets-database.herokuapp.com`
 
 //const RemoteURL = "http://localhost:8081"
 
-//and aother change
-
 export default {
     getOne: (id, db) => {
         return fetch(`${RemoteURL}/${db}/${id}`)
