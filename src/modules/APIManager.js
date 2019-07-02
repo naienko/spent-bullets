cd65fccffd704b4bea590326357d3f1af403db21
Panya Farnette <naienko@gmail.com>
2019-07-02 15:39:18 -0500

    prepping for full conversion -- protecting old files

--- src/modules/APIManager.js
+++ src/modules/APIManager.js
@@ -1,57 +1,63 @@
-//const RemoteURL = "https://bullets-database.herokuapp.com";
+const mysql = require('mysql');
 
-const RemoteURL = "http://localhost:8081"
+class Database {
+	constructor() {
+		this.connection = mysql.createConnection({
+			//remember when going to live to insert live connection data here
+			host: "localhost",
+			port: "3311",
+			user: "root",
+			password: "h1s3k117",
+			database: "spent_bullets"
+		});
+	}
+	query(sql, args) {
+		return new Promise((resolve, reject) => {
+			this.connection.query(sql, args, (err, rows) => {
+				if (err)
+					return reject(err);
+				resolve(rows);
+			});
+		});
+	}
+	close() {
+		return new Promise((resolve, reject) => {
+			this.connection.end(err => {
+				if (err)
+					return reject(err);
+				resolve();
+			});
+		});
+	}
+}
 
 export default {
-    getOne: (id, db) => {
-        return fetch(`${RemoteURL}/${db}/${id}`)
-            .then(results => results.json())
-    },
-    getAll: function (db) {
-        return fetch(`${RemoteURL}/${db}`)
-            .then(results => results.json())
-    },
-    getQuery: (query, db) => {
-        return fetch(`${RemoteURL}/${db}/?${query}`)
-            .then(results => results.json())
-    },
-    getOneQuery: (id, query, db) => {
-        return fetch(`${RemoteURL}/${db}/${id}/?${query}`)
-            .then(results => results.json())
-    },
-    delete: (id, db) => {
-        return fetch(`${RemoteURL}/${db}/${id}`, {
-            method: "DELETE"
-        })
-    },
-    add: (db, newObject) => {
-        return fetch(`${RemoteURL}/${db}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newObject)
-        })
-        .then(results => results.json())
-    },
-    update: (db, newObject, id) => {
-        return fetch(`${RemoteURL}/${db}/${id}`, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newObject)
-        })
-        .then(results => results.json())
-    },
-    edit: (db, newObject, id) => {
-        return fetch(`${RemoteURL}/${db}/${id}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newObject)
-        })
-        .then(results => results.json())
-    }
+	getOne: (id, db) => {
+		return Database.query(`SELECT * FROM ${db} WHERE id = ${id}`)
+			.then(results => results.json())
+	},
+	getAll: function (db) {
+		return Database.query(`SELECT * FROM ${db}`)
+			.then(results => results.json())
+	},
+	//DELETE method
+	delete: (id, db) => {
+		return Database.query(`DELETE FROM ${db} WHERE id = ${id}`)
+	},
+	//POST method
+	//make this check if the table is users, then hash the password?
+	add: (db, newObject) => {
+		return Database.query(`INSERT INTO ${db} SET ?`, newObject)
+			.then(results => results.json())
+	},
+	//PUT method
+	update: (db, newObject, id) => {
+		return Database.query(`UPDATE ${db} SET ? WHERE id = ${id}`, newObject)
+			.then(results => results.json())
+	}
+	
+	//figure out how to do the query methods
+	//queries appear in StackManager, ApplicationViews, Register, Login
+	//shove queries into querymanager?
+	//fix users db to default role to 'user'
 }
